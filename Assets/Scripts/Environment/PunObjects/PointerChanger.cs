fdbff3ea273f9d51aa5ff922313f5d681b5401d2
EndocrineMSc <bamsc.studio@gmail.com>
2024-01-27 13:28:10 +0100

    Pointer now changes above interactable objects
    Used a temporary test-asset, that requires attribution to author -> delete as soon as we have our own asset.

--- Assets/Scripts/Environment/PunObjects/PointerChanger.cs
+++ Assets/Scripts/Environment/PunObjects/PointerChanger.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PointerChanger : MonoBehaviour
+public class PointerChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Fields and Properties
+
+    [SerializeField] private Texture2D _cursorTexture;
+
+    #endregion
+
+    #region Methods
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        Cursor.SetCursor(_cursorTexture, Vector2.zero, CursorMode.Auto);    
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);    
     }
+
+    #endregion
 }
