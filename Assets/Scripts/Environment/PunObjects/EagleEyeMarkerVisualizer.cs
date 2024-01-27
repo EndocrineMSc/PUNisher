61f8b22d1ab9e2a78b847d3a006a95ebf745a218
EndocrineMSc <bamsc.studio@gmail.com>
2024-01-27 13:14:51 +0100

    Add eagle eye marker sytem to visualize interactable objects

---
+++ Assets/Scripts/Environment/PunObjects/EagleEyeMarkerVisualizer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    #region Fields and Properties
+
+    [SerializeField] private Image _eagleEyeMarker;
+
+    #endregion
+
+    #region Methods
+
+    void Start() {
+        _eagleEyeMarker.enabled = false;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _eagleEyeMarker.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            _eagleEyeMarker.enabled = false;
+        }
+    }
+
+    #endregion
+}
